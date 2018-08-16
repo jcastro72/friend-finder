0acfcae442d7b141c17e3cfc072f0586c8b17951
jcastro15 <castrojake99@gmail.com>
2018-08-16 14:19:34 -0400

    First Commit

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [ {
+    "name" : "Dwight",
+    "photo" : "https://pbs.twimg.com/profile_images/549268771484229632/WnatiHzT_400x400.jpeg" ,
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Jim",
+    "photo" : "https://img.buzzfeed.com/buzzfeed-static/static/2017-08/9/11/asset/buzzfeed-prod-fastlane-01/sub-buzz-22381-1502291405-3.jpg?downsize=715:*&output-format=auto&output-quality=auto" ,
+    "scores" : [4,3,2,1,5,4,3,3,2,1]
+}, {
+    "name" : "Pam",
+    "photo" : "https://theofficeanalytics.files.wordpress.com/2017/09/pam-halpert-1.jpg?w=1200" ,
+    "scores" : [3,2,3,4,5,2,3,4,5,5]
+}, {
+    "name" : "Michael",
+    "photo" : "https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png" ,
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+},{
+    "name" : "Jan",
+    "photo" : "https://vignette.wikia.nocookie.net/theoffice/images/b/b9/151564.jpg/revision/latest?cb=20170724042647" ,
+    "scores" : [3,2,1,1,4,5,3,2,1,1]
+}, {
+    "name" : "Angela",
+    "photo" : "https://consequenceofsound.files.wordpress.com/2017/12/nup-101558-0386.jpg?quality=80" ,
+    "scores" : [4,4,4,3,1,1,1,2,3,5]
+}, {
+    "name" : "Andy",
+    "photo" : "https://vignette.wikia.nocookie.net/theoffice/images/b/b5/Andy_Bernard.jpg/revision/latest?cb=20170701084205" ,
+    "scores" : [2,2,2,4,3,5,2,1,2,3]
+}];
+
+module.exports = friends;
